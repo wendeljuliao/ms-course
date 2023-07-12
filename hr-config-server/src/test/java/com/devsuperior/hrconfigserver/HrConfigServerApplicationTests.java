7bcb4c8fe12a7274b05809708f9c716adf1a14f6
Wendel <wendelljuliao@hotmail.com>
2023-07-12 15:50:58 -0300

    aplicação feita

--- hr-config-server/src/test/java/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
@@ -1,13 +1,19 @@
 package com.devsuperior.hrconfigserver;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrConfigServerApplicationTests {
+class HrConfigServerApplicationTests implements CommandLineRunner {
 
-	@Test
-	void contextLoads() {
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("USERNAME: " + username);
+		
 	}
 
 }
---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.hroauth.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		
+		if (user == null) {
+			throw new IllegalArgumentException("Email not found");
+		}
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		return user;
+	}
+
+}
